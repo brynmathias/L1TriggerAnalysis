5152e6ee0b2d3b0c3b1f761231ee98ce1df58031
Bryn Mathias <brynmathias@gmail.com>
2012-06-21 11:03:45 +0100

    update the turn on code a little to produce multi turn ons in one plot

--- TurnOns.py
+++ TurnOns.py
@@ -4,36 +4,62 @@ from plottingUtils import *
 
 
 turnOns = {
- "RefJet":["JetEt10","JetEt20","JetEt30","JetEt40","JetEt50","JetEt60","JetEt70","JetEt80","JetEt90","JetEt100","JetEt110","JetEt120","JetEt130","JetEt140","JetEt150"],
+
+
+
+#"Denomiantor":[List of numerators]  
+ "RefJet":["Jet16","Jet36","Jet52","Jet92",],
+ "RecoHT":["RecoHTL150","RecoHTL175","RecoHTL1100","RecoHTL1150",],
+# Special set so that we dont need to have a different loop for drawing 2d histos as well as other 1d distros
   "NoRatio":["EnCorrelation","L1HT",],
-  "RecoHT":["RecoHTL1100","RecoHTL1150","RecoHTL175","RecoHTL150","RecoHTL1175","RecoHTL1125",]
+  "METReference":["MET30Pass","MET50Pass","MET70Pass"]
+
 }
 
 
-fname = "SingleMu_L1Trigger.root"
+fname = "./rootFiles/test.root"
 
 def main():
   """docstring for main"""
   c1 = Print("out.pdf")
+  c1.DoPageNum = False
   c1.open()
   for key,triggerL in turnOns.iteritems():
-
+    turnOnList = []
     c1.canvas.SetLogy(False)
     f = r.TFile.Open(fname)
     if "NoRatio" not in key:
       denominator = f.Get(key)
       # if "HT"  in key:
       denominator.Rebin(10)
+      c = 1
       for trig in triggerL:
-
+        if c == 5: c+=1
         numerator = f.Get(trig)
         # if "HT" in key:
         numerator.Rebin(10)
         TurnOn = r.TGraphAsymmErrors()
-        TurnOn.SetTitle(trig)
+        # TurnOn.SetTitle(trig)
         TurnOn.Divide(numerator,denominator)
+        TurnOn.SetMarkerColor(c)
+        TurnOn.SetLineColor(c)
+        c+=1
         TurnOn.Draw("ap")
+        turnOnList.append(TurnOn)
         c1.Print()
+      c1.Clear()
+      mg = r.TMultiGraph()
+      for g in turnOnList:
+        mg.Add(g)
+      leg = Legend(x1= 0.5, y1=0.25, x2 = 0.8, y2 = 0.8)
+      for g,name in zip(turnOnList,triggerL):
+        leg.AddEntry(g,name,"pl")
+      mg.Draw("ap")
+      mg.GetXaxis().SetRangeUser(0.,300.)
+      mg.GetXaxis().SetTitle("E_{T} (GeV)")
+      mg.GetYaxis().SetTitle("Efficiency")
+      leg.Draw()
+      c1.Print()
     else:
       for hist in triggerL:
         hist = f.Get(hist)
